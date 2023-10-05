72e6792b703da7ae3dc9b7bda8aebe208154065e
Adnan <adnan.hussain.136660@gmail.com>
2023-10-05 16:28:53 +0530

    Initial Commit

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "Info Pulse",
+    description:
+        "Stay updated with the latest breaking news, trends, and stories from around the world on Info Pulse.",
+};
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
+}
