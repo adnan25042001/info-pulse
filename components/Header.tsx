3ac026dac1ffef91ea41d20eb0decf461583ca69
Adnan <adnan.hussain.136660@gmail.com>
2024-02-12 21:51:01 +0530

    added loader and updated navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,7 @@ import DarkModeButton from "./DarkModeButton";
 const Header = () => {
     return (
         <header className="max-w-6xl mx-auto px-3">
-            <div className="flex py-10 items-center justify-between">
+            <div className="flex py-5 sm:py-7 md:py-10 items-center justify-between">
                 <Link href="/" prefetch={false}>
                     <h1 className="font-serif text-4xl text-center underline decoration-4 decoration-orange-500">
                         InfoPulse
