a1b604249bbba39f37d7eb2020581a35ee32f9ea
Adnan <adnan.hussain.136660@gmail.com>
2024-02-13 15:27:26 +0530

    updated read more button

--- components/ReadMoreButton.tsx
+++ components/ReadMoreButton.tsx
@@ -1,30 +1,17 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 type Props = {
     article: Article;
 };
 
 const ReadMoreButton = ({ article }: Props) => {
-    const router = useRouter();
-
-    const handleClick = () => {
-        const queryString = Object.entries(article)
-            .map(([key, value]) => `${key}=${value}`)
-            .join("&");
-
-        const url = `/article?${queryString}`;
-
-        router.push(url);
-    };
-
     return (
-        <button
-            onClick={handleClick}
-            className="bg-orange-400 h-10 rounded-b-lg dark:text-gray-900 hover:bg-orange-500"
-        >
-            Read More
+        <button className="bg-orange-400 h-10 rounded-b-lg dark:text-gray-900 hover:bg-orange-500">
+            <Link href={article.url} target="_blank">
+                Read More
+            </Link>
         </button>
     );
 };
